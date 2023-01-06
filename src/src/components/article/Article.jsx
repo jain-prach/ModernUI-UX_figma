29c9623943948c4a666ea5c8fc2a07dab155b4b3
jain-prach <76729905+jain-prach@users.noreply.github.com>
2023-01-06 20:56:36 +0530

    whatGPT3, Features, Possibility and CTA done

---
+++ src/src/components/article/Article.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './article.css'; //importing stylesheet
+const Article = () => {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article
---
+++ src/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root')); //this app component is going to be rendered inside of a div with ID = root
+
